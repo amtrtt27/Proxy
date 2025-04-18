e392006807a62ed4063bc62301b53657c89b5943
amtrtt27 <tntran@andrew.cmu.edu>
2025-04-17 20:24:23 -0400

    doit

--- proxy.c
+++ proxy.c
@@ -1,13 +1,21 @@
 /*
  * Starter code for proxy lab.
  * @author Tram Tran <tntran@andrew.cmu.edu>
- * Feel free to modify this code in whatever way you wish.
+ * 
+ * This proxy is responsible for:
+ * - Accept and parse incoming requests from clients.
+ * - Forward these requests to the end server.
+ * - Transfer the data from the end server back to the client.
+ * 
+ * To handle multiple requests at once, the proxy uses threads.
+ * It listens for client connections, and upon accepting one, it creates a
+ * separate connection file for each client socket and does the work.
+ * 
+ * Reference: Textbook
  */
 
-/* Some useful includes to help you get started */
-
 #include "csapp.h"
-
+#include "http_parser.h"
 #include <assert.h>
 #include <ctype.h>
 #include <inttypes.h>
@@ -38,7 +46,14 @@
 #define dbg_printf(...)
 #endif
 
+/* Global Variable */
+#define DEFAULT_PORT "80"
+#define SA struct sockaddr
 
+void clienterror(int fd, const char *errnum, const char *shortmsg, const char *longmsg);
+int main(int argc, char **argv);
+void *thread(void *vargp);
+void doit(int cliend_fd);
 /*
  * String to use for the User-Agent header.
  * Don't forget to terminate with \r\n
@@ -95,37 +110,39 @@ void clienterror(int fd, const char *errnum, const char *shortmsg,
     }
 }
 
-
 /**
- * Code reference: Textbook
+ * @brief handles primary workflow of the proxy:
+ * - Acts as a server: create a listening socket to accept HTTP requests from 
+ * the browser.
+ * - Acts as a client: forward those requests to the end server.
  */
 int main(int argc, char **argv) {
     int listenfd;
     int *connfd;
-    char hostname[MAXLINE], port[MAXLINE];
-    socketlen_t clientlen;
+    char hostname[MAXLINE], client_port[MAXLINE];
+    socklen_t clientlen;
     struct sockaddr_storage clientaddr;
-    pthread_t tid;
+    
 
-    if (arg != 2) {
+    if (argc != 2) {
         fprintf(stderr, "usage: %s <domain name>\n", argv[0]);
         exit(1);
     }
     
     /* Ignore SIGPIPE to prevent crashes when client closes connection early */
-    Signal(SIGPIPE, ISG_IGN);
+    Signal(SIGPIPE, SIG_IGN);
 
-    /* argv[1] is port number */
+    /* port number */
     listenfd = open_listenfd(argv[1]);
 
     while (1) {
-        clientlen = sizeof(clientaddr);
+        clientlen = sizeof(struct sockaddr_storage);
         connfd = Malloc(sizeof(int));
-        *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        *connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);
 
-        getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
-        printf("Accepted connection from (%s, %s)\n", hostname, port);
-
+        getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, client_port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, client_port);
+        pthread_t tid;
         if (pthread_create(&tid, NULL, thread, connfd) != 0) {
             perror("Pthread create error");
             close(*connfd);
@@ -136,14 +153,14 @@ int main(int argc, char **argv) {
 }
 
 /**
- * @brief
+ * @brief handles each connection using a separate thread
  */
 void *thread (void *vargp) {
     int connfd = *((int*) vargp);
-    Pthread_detach(pthread_self());
+    pthread_detach(pthread_self());
     Free(vargp);
     doit(connfd);
-    Close(connfd);
+    close(connfd);
     return NULL;
 }
 
@@ -156,43 +173,95 @@ void *thread (void *vargp) {
  * - Forwards the end server's response back to the client
  */
 void doit (int client_fd) {
-    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-    char path[MAXLINE], hostname[MAXLINE];
-
-    rio_t rio;
-    rio_t rio_end;
+    char buf[MAXLINE];
     parser_t *parser = parser_new();
+    rio_t client_rio, server_rio;
+    const char *method, *path, *hostname, *port, *uri;
+
+    int server_fd;
     parser_state parse_state;
 
-    if (!parser) {
-        clienterror(clientfd, "400", "Bad request", "Parser Failed!");
+    if (parser == NULL) {
+        clienterror(client_fd, "400", "Bad request", "Parser Failed!");
         return;
     }
 
-    rio_readinitb(&rio, client_fd);
+    rio_readinitb(&client_rio, client_fd);
 
     /* Reach EOF or error occured */
-    if (!rio_readlineb(&rio, buf, MAXLINE)) {
+    if (!rio_readlineb(&client_rio, buf, MAXLINE)) {
         parser_free(parser);
         return;
     }
 
-    /* Parsing */
     parse_state = parser_parse_line(parser, buf);
 
     if (parse_state != REQUEST) {
         parser_free(parser);
-        clienterror(clientfd, "400", "Bad request", "Parse fail");
+        clienterror(client_fd, "400", "Bad request", "Parse fail");
         return;
     }
-
+    
+    parser_retrieve(parser, METHOD, &method);
+    parser_retrieve(parser, URI, &uri);
+    parser_retrieve(parser, HOST, &hostname);
+    parser_retrieve(parser, PORT, &port);
+    parser_retrieve(parser, PATH, &path);
     
     
+    if (!port) port = DEFAULT_PORT;
     
+    if (method == NULL || path == NULL || hostname == NULL) {
+        clienterror(client_fd, "400", "Bad request", "Invalid header");
+        parser_free(parser);
+        return;
+    }
     /* Invalid request */
     if (strcasecmp(method, "GET")) {
-        clienterror(client_fd, method, "501", "Not implemented", "Tiny does not support this method");
+        parser_free(parser);
+        clienterror(client_fd, "501", "Not implemented", "Tiny does not support this method");
         return;
     }
 
+    /* Connect to end server */
+    server_fd = open_clientfd(hostname, port);
+
+    snprintf(buf, sizeof(buf), "GET %s HTTP/1.0\r\n", path);
+    rio_writen(server_fd, buf, strlen(buf));
+
+    while (rio_readlineb(&client_rio, buf, sizeof(buf)) > 0) {
+        if (strcmp(buf, "\r\n") == 0) break;
+
+        if (strncasecmp(buf, "Host:", 5) == 0) {
+            rio_writen(server_fd, buf, strlen(buf));
+            hostname = NULL;
+            continue;
+        }
+
+        if (strncasecmp(buf, "Connection:", 11) == 0 || strncasecmp(buf, "Proxy-Connection:", 17) == 0 || strncasecmp(buf, "User-Agent:", 11) == 0) continue;
+
+        rio_writen(server_fd, buf, strlen(buf));
+    }
+
+    if (hostname) {
+        snprintf(buf, sizeof(buf), "Host: %s:%s\r\n", hostname, port);
+        rio_writen(server_fd, buf, strlen(buf));
+    }
+    rio_readinitb(&server_rio, server_fd);
+    snprintf(buf, sizeof(buf), "User-Agent: %s\r\n", header_user_agent);
+    rio_writen(server_fd, buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "Connection: close\r\n");
+    rio_writen(server_fd, buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "Proxy-Connection: close\r\n");
+    rio_writen(server_fd, buf, strlen(buf));
+
+    snprintf(buf, sizeof(buf), "\r\n");
+
+    rio_readinitb(&server_rio, server_fd);
+    ssize_t num_bytes;
+    while ((num_bytes = rio_readnb(&server_rio, buf, sizeof(buf))) > 0) {
+        rio_writen(client_fd, buf, num_bytes);
+    }
+    close(server_fd);
+    parser_free(parser);
 }
